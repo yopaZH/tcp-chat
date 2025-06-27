84a105daeab53bd61570acadb7912d0437c3f589
styopa kolarzh <styopa.kolarzh@gmail.com>
2025-06-28 00:00:01 +0300

    fix: added sender name string to protocol, to not request name separately from server

--- common/types.go
+++ common/types.go
@@ -1,9 +1,5 @@
 package common
 
-import (
-	"tcp-chat/transport"
-)
-
 type ChatConnection interface {
 	Read([]byte) (int, error)
 	Write([]byte) (int, error)
@@ -13,14 +9,12 @@ type ChatConnection interface {
 type User struct {
 	Id   uint64
 	Name string
-	Conn transport.Connection
-
-	Send chan Message
 }
 
 type Message struct {
-	From    uint64
-	To      uint64
-	Type    MessageType
-	Content string
+	FromName string
+	From     uint64
+	To       uint64
+	Type     MessageType
+	Content  string
 }
