f61cd554ce62d596db654fb1cf62e81622f018d1
styopa kolarzh <styopa.kolarzh@gmail.com>
2025-06-28 00:01:52 +0300

    fix: port changed to addreses

--- config/config.go
+++ config/config.go
@@ -6,14 +6,23 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-// Config структура для всего конфига
 type Config struct {
 	Server ServerConfig `yaml:"server"`
+	Client ClientConfig `yaml:"client"`
 }
 
 type ServerConfig struct {
 	//Host string `yaml:"host"`
-	Port string `yaml:"port"`
+	//Port string `yaml:"port"`
+	Address string `yaml:"address"`
+	//ReadTimeoutSec int `yaml:"read_timeout_sec"`
+	//WriteTimeoutSec int `yaml:"write_timeout_sec"`
+}
+
+type ClientConfig struct {
+	//Host string `yaml:"host"`
+	//Port string `yaml:"port"`
+	Address string `yaml:"address"`
 	//ReadTimeoutSec int `yaml:"read_timeout_sec"`
 	//WriteTimeoutSec int `yaml:"write_timeout_sec"`
 }
