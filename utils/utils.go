b1ece38c46f0a651da4021a26b14975245fb7879
styopa kolarzh <styopa.kolarzh@gmail.com>
2025-06-28 13:42:51 +0300

    feat: added MakeChatKey utility

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,15 @@
 package utils
 
+import "fmt"
+
 func Exists(id uint64, set map[uint64]struct{}) bool {
 	_, ex := set[id]
 	return ex
+}
+
+func MakeChatKey(userA, userB uint64) string {
+	if userA > userB {
+		userA, userB = userB, userA
+	}
+	return fmt.Sprintf("%d_%d", userA, userB)
 }
